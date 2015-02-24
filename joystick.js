d117ac0da617c67132a809389f37d42268fc8cb4
bgardon <benbwgardon@gmail.com>
2015-02-23 19:36:55 -0800

    reworked jotstick.html

--- joystick.js
+++ joystick.js
@@ -1,11 +1,74 @@
 function init() {
-    var body = document.getElementsByTagName('body')[0];
-	body.addEventListener('keydown', function(e) {
-	  	handleKey(e.keyCode, true);
-	}, false);
-	body.addEventListener('keyup', function(e) {
-	  	handleKey(e.keyCode, false);
-	}, false);
+	console.log("touchscreen is", VirtualJoystick.touchScreenAvailable() ? "available" : "not available");
+	
+	var joystickL	= new VirtualJoystick({
+		mouseSupport: true,
+		stationaryBase: true,
+        baseX: 200,
+        baseY: 200,
+		
+		container	: document.getElementById("joystick-left"),
+		strokeStyle	: 'cyan',
+		stickRadius	: 120		
+	});
+	joystickL.addEventListener('touchStartValidation', function(event){
+		var touch	= event.changedTouches[0];
+		if( touch.pageX < window.innerWidth/2 )	return false;
+		return true
+	});
+
+	// one on the right of the screen
+	var joystickR	= new VirtualJoystick({
+		mouseSupport: true,
+		stationaryBase: true,
+        baseX: 200,
+        baseY: 200,
+		
+		container	: document.getElementById("joystick-right"),
+		strokeStyle	: 'orange',
+		stickRadius	: 120		
+	});
+	joystickR.addEventListener('touchStartValidation', function(event){
+		var touch	= event.changedTouches[0];
+		if( touch.pageX >= window.innerWidth/2 )	return false;
+		return true
+	});
+
+
+
+
+	
+	// joystickL.addEventListener('touchStart', function(){
+	// 	console.log('down')
+	// })
+	// joystickL.addEventListener('touchEnd', function(){
+	// 	console.log('up')
+	// })
+	// joystickR.addEventListener('touchStart', function(){
+	// 	console.log('down')
+	// })
+	// joystickR.addEventListener('touchEnd', function(){
+	// 	console.log('up')
+	// })
+
+	setInterval(function(){
+		var outputEl	= document.getElementById('result');
+		outputEl.innerHTML	= '<b>Result:</b> '
+			+ 'Left:'
+			+ ' dx:'+joystickL.deltaX()
+			+ ' dy:'+joystickL.deltaY()
+			+ (joystickL.right()	? ' right'	: '')
+			+ (joystickL.up()	? ' up'		: '')
+			+ (joystickL.left()	? ' left'	: '')
+			+ (joystickL.down()	? ' down' 	: '')
+			+ 'Right:'
+			+ ' dx:'+joystickR.deltaX()
+			+ ' dy:'+joystickR.deltaY()
+			+ (joystickR.right()	? ' right'	: '')
+			+ (joystickR.up()	? ' up'		: '')
+			+ (joystickR.left()	? ' left'	: '')
+			+ (joystickR.down()	? ' down' 	: '')	
+	}, 1/30 * 1000);
 }
 
 window.onload = init;
